978b2480473d3cdab59551bb851c2dc08338c8f9
Jaellynda <jaellynda6@gmail.com>
2026-05-15 15:55:53 +0300

    Fix: remove duplicate lowercase authContext import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
-import { AdminAuthProvider } from '@/lib/authContext';
+
 
 // Layout
 import AppLayout from './components/schp/AppLayout';
